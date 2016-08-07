64af4ce366e55d89c16da66e70301a4e78e23710
Andrew Simpkins <andrewsimpkins@gmail.com>
2016-08-07 11:12:56 -0700

    Update version

--- scripts/build.bat
+++ scripts/build.bat
@@ -6,5 +6,5 @@ SET JAVA_HOME=C:\Program Files\Java\jdk1.8.0
 SET PATH=%CD%\maven\apache-maven-3.2.5\bin;%JAVA_HOME%\bin;%PATH%
 SET MAVEN_OPTS=-XX:MaxPermSize=2g -Xmx4g
 SET JAVA_OPTS=-XX:MaxPermSize=2g -Xmx4g
-scripts\xml pom.xml SET /empty:project/empty:version 1.0.0
+scripts\xml pom.xml SET /empty:project/empty:version %APPVEYOR_BUILD_VERSION%
 mvn clean package --batch-mode -DskipTest
