39993cc24ccf84fc0691bf84fde80b1863d65a77
Andrew Simpkins <andrewsimpkins@gmail.com>
2016-08-12 17:19:34 -0700

    update build script

--- scripts/build.bat
+++ scripts/build.bat
@@ -7,6 +7,6 @@ SET PATH=%CD%\maven\apache-maven-3.2.5\bin;%JAVA_HOME%\bin;%PATH%
 REM SET MAVEN_OPTS=-XX:MaxPermSize=2g -Xmx4g
 REM SET JAVA_OPTS=-XX:MaxPermSize=2g -Xmx4g
 REM scripts\xml pom.xml SET /empty:project/empty:version %APPVEYOR_BUILD_VERSION%
-ant\bin\ant
+ant\apache-ant-1.9.7\bin\ant
 REM move target\simpkins-query-%APPVEYOR_BUILD_VERSION%.jar simpkins-query.jar
 jar cvf simpkins-query.jar out\production\simpkins-query
